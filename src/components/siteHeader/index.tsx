b0c9476234977ba47ca8fb3dd7a0223152d2840c
Lovely Gregorio <lovelymetos@gmail.com>
2026-07-09 02:00:37 +0100

    Added Upcoming movies page.

--- src/components/siteHeader/index.tsx
+++ src/components/siteHeader/index.tsx
@@ -30,8 +30,8 @@ const SiteHeader: React.FC = () => {
   const menuOptions = [
     { label: "Home", path: "/" },
     { label: "Favorites", path: "/movies/favourites" },
-    { label: "Option 3", path: "/" },
-    { label: "Option 4", path: "/" },
+    { label: "Upcoming", path: "/movies/upcoming" },
+
   ];
 
   const handleMenuSelect = (pageURL: string) => {
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
